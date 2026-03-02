943c1dbc7be9b745da61b739200ef0f2447038d3
Tishaya <tishaya.thompson@outlook.com>
2026-03-02 15:12:35 -0500

    Add project files.

---
+++ Kiyomi's Instant Pet/ModEntry.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using HarmonyLib;
+using Microsoft.Xna.Framework;
+using StardewModdingAPI;
+using StardewModdingAPI.Events;
+using StardewModdingAPI.Utilities;
+using StardewValley;
+
+namespace Kiyomi_s_Instant_Pet
+{
+    internal class ModEntry
+    {
+    }
+}
